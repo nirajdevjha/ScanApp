64963517674db386e45923c06c046c8807934483
Niraj <niraj.jha@go-mmt.com>
2020-09-27 13:21:07 +0530

    Initial Commit

---
+++ ScanApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScanApp
+//
+//  Created by Niraj Kumar Jha on 27/09/20.
+//  Copyright © 2020 Niraj Kumar Jha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
